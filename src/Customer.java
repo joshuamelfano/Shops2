ecc975697f33c40b1fa79130c9ebb795e7a2b4e7
Joshua <v.joshua.kayadoe@adira.co.id>
2025-05-19 23:37:06 +0700

    test

--- src/Customer.java
+++ src/Customer.java
@@ -3,4 +3,20 @@ public class Customer {
 
     protected Item[] shoppingBag;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Item[] getShoppingBag() {
+        return shoppingBag;
+    }
+
+    public void setShoppingBag(Item[] shoppingBag) {
+        this.shoppingBag = shoppingBag;
+    }
+
 }
--- src/Shop.java
+++ src/Shop.java
@@ -1,15 +1,45 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Shop {
-    protected ArrayList<Customer> customers;
+    protected ArrayList<Customer> customers = new ArrayList<>();
     protected ArrayList<Item> items;
+    Scanner sc = new Scanner(System.in);
 
     public Shop() {
-        Scanner sc = new Scanner(System.in);
+        String nameInput="";
+        String optionInput = "";
+        while(!nameInput.equalsIgnoreCase("exit")) {
+            System.out.println("Please Type the Customer name: (Type exit to finish populating customers");
+            nameInput = sc.next();
+            populateCustomers(nameInput);
+            System.out.println("Registered user list");
+            for(int i = 0; i<customers.size(); i++){
+                System.out.println(customers.get(customers.size()-1).getName());
+            }
+        }
 
     }
 
-    // customerCome()
+    public void populateCustomers(String name){
+
+
+        Customer c = new Customer();
+        c.setName(name);
+        customers.add(c);
+
+    }
+
+    public void customerCome(String name){
+        System.out.println("Customer Come Menu");
+
+
+
+    }
+
+    public void registerCustomer(String name){
+
+
+    }
     // customerLeave()
     // addProduct()
     // removeProduct()
